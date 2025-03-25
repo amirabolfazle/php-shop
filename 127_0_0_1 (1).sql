c3ac1dd6cb81d45b3948f2d541d282e938b1df3f
amirabolfazl <amirabolfazl.alipour@hotmail.com>
2025-03-26 00:51:34 +0330

    add sql file:)

---
+++ 127_0_0_1 (1).sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 16, 2024 at 04:15 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hamstershop`
+--
+CREATE DATABASE IF NOT EXISTS `hamstershop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `hamstershop`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Lord'),
+(2, 'Master');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_products`
+--
+
+CREATE TABLE `category_products` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category_products`
+--
+
+INSERT INTO `category_products` (`id`, `category_id`, `product_id`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `about` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `off` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `about`, `price`, `off`) VALUES
+(1, 'اکانت وریفای شده', 'دارای پرفیت و کوین بالا', 2000000, 2),
+(7, '2', '2', 2, 0),
+(8, '2', '2', 2, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category_products`
+--
+ALTER TABLE `category_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `category_products`
+--
+ALTER TABLE `category_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- admin/product/action.php
+++ admin/product/action.php
@@ -5,5 +5,14 @@
     $connect=mysqli_connect('localhost','root','','hamstershop');
     $sql="INSERT INTO `products` (`name`, `about`,`price`) VALUES ('$name', '$about','$price');";
     $result=mysqli_query($connect,$sql);
+    $sql_2 = "select * from products;";
+    $resault_2 = mysqli_query($connect, $sql_2);
+    if (mysqli_num_rows($resault_2)>0){
+        while ($row=mysqli_fetch_assoc($resault_2)){
+            $id=$row['id'];
+        }
+    }
+    $sql3="INSERT INTO `category_products` (`id`, `category_id`, `product_id`) VALUES (NULL, '1', '$id');";
+    $result3=mysqli_query($connect,$sql3);
     header("Location: index.php")
 ?>
